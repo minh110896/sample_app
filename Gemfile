c7dbb6e19e769259183bfd132b21c60bac7530d5
Hoang Minh <hoangminh11081996@gmail.com>
2017-09-22 09:09:05 +0700

    Finish chapter 9

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem "remotipart"
 gem "jquery-turbolinks"
 gem "jquery"
 gem "jquery-rails"
+gem "bootstrap_form"
 # Use sqlite3 as the database for Active Record
 gem "sqlite3"
 # Use Puma as the app server
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,3 +9,11 @@ class ActiveSupport
     end
   end
 end
+
+class ActionDispatch
+  class IntegrationTest
+    def log_in_as user, password: "password", remember_me: "1"
+      post login_path, params: {session: {email: user.email, password: password, remember_me: remember_me}}
+    end
+  end
+end
